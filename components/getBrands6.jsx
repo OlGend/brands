323a9cd0ffc49bd1c7204bd4ef9b63cc2383e19f
Oleh <gend4016@gmail.com>
2024-10-09 11:29:11 +0300

    brands6

--- components/getBrands6.jsx
+++ components/getBrands6.jsx
@@ -13,9 +13,11 @@ export const getBrands = async (y) => {
     } else {
       console.error("Failed to fetch data:", res.status);
       return []; // Возвращаем пустой массив в случае ошибки
+      
     }
   } catch (error) {
     console.error("An error occurred:", error);
     return []; // Возвращаем пустой массив в случае ошибки
   }
 };
+
